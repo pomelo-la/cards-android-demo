3e184d970244df9f6ace581224deace210292027
diegoiribarne <diribarne@ovloop.com>
2023-01-06 12:52:24 -0300

    simplify design on compose

--- app/src/main/java/com/demo/cards/pomelo/CardComposeScreen.kt
+++ app/src/main/java/com/demo/cards/pomelo/CardComposeScreen.kt
@@ -39,17 +39,12 @@ import com.pomelo.cards.ui.pin.change.PomeloChangePinComposable
 
 @Suppress("LongMethod", "ComplexMethod")
 @Composable
+
 fun CardComposeScreen() {
     val scaffoldState: ScaffoldState = rememberScaffoldState()
     var showSensitiveCardData by rememberSaveable { mutableStateOf(false) }
     var showCardBottomSheet by remember { mutableStateOf(false) }
 
-    var changePin by remember { mutableStateOf(false) }
-    var activate by remember { mutableStateOf(false) }
-    var changePinText by remember { mutableStateOf("CAMBIO DE PIN") }
-    var activateText by remember { mutableStateOf("ACTIVAR") }
-
-    val context = LocalContext.current
     MdcTheme {
         Scaffold(
             scaffoldState = scaffoldState,
@@ -105,82 +100,35 @@ fun CardComposeScreen() {
                         }
                     }
 
-                    Divider(Modifier.padding(top = 16.dp, bottom = 16.dp))
-                    AnimatedVisibility(visible = !activate) {
-                        Button(
-                            modifier = Modifier
-                                .padding(start = 8.dp)
-                                .fillMaxWidth(),
-                            onClick = {
-                                activate = true
-                            }
-                        ) {
-                            Text(
-                                text = activateText,
-                                style = MaterialTheme.typography.button
-                            )
-                        }
-                    }
-
-                    AnimatedVisibility(
-                        visible = activate,
-                        enter = expandVertically(),
-                        exit = shrinkVertically()
-                    ) {
-                        PomeloActivateCardView(
-                            scaffoldState = scaffoldState,
-                            onResultListener = { cardsResult, _ ->
-                                when (cardsResult) {
-                                    CardsResult.NETWORK_ERROR -> {
-                                    }
-                                    CardsResult.BIOMETRIC_ERROR -> {
-                                    }
-                                    CardsResult.SUCCESS -> {
-                                        activateText = context.getString(R.string.success_activate)
-                                        activate = false
-                                    }
+                    PomeloActivateCardView(
+                        scaffoldState = scaffoldState,
+                        onResultListener = { cardsResult, _ ->
+                            when (cardsResult) {
+                                CardsResult.NETWORK_ERROR -> {
+                                }
+                                CardsResult.BIOMETRIC_ERROR -> {
+                                }
+                                CardsResult.SUCCESS -> {
                                 }
                             }
-                        )
-                    }
-                    AnimatedVisibility(visible = !changePin) {
-                        Button(
-                            modifier = Modifier
-                                .padding(start = 8.dp, top = 16.dp)
-                                .fillMaxWidth(),
-                            onClick = {
-                                changePin = true
-                            }
-                        ) {
-                            Text(
-                                text = changePinText,
-                                style = MaterialTheme.typography.button
-                            )
                         }
-                    }
+                    )
 
-                    AnimatedVisibility(
-                        visible = changePin,
-                        enter = expandVertically(),
-                        exit = shrinkVertically()
-                    ) {
-                        PomeloChangePinComposable(
-                            cardId = BuildConfig.CARD_ID,
-                            scaffoldState = scaffoldState,
-                            onResultListener = { cardsResult, message ->
-                                when (cardsResult) {
-                                    CardsResult.NETWORK_ERROR -> {
-                                    }
-                                    CardsResult.BIOMETRIC_ERROR -> {
-                                    }
-                                    CardsResult.SUCCESS -> {
-                                        changePinText = context.getString(com.demo.cards.pomelo.R.string.success_pin)
-                                        changePin = false
-                                    }
+                    PomeloChangePinComposable(
+                        cardId = BuildConfig.CARD_ID,
+                        scaffoldState = scaffoldState,
+                        onResultListener = { cardsResult, message ->
+                            when (cardsResult) {
+                                CardsResult.NETWORK_ERROR -> {
+                                }
+                                CardsResult.BIOMETRIC_ERROR -> {
+                                }
+                                CardsResult.SUCCESS -> {
+
                                 }
                             }
-                        )
-                    }
+                        }
+                    )
                 }
 
                 PomeloCardBottomSheet(
